134263a27af51055245987d94cade05374624b60
chikenH <chikenh.aoi@gmail.com>
2023-05-08 23:01:23 +0900

    update AssemblyVersion

--- CountdownTimerTest/Properties/AssemblyInfo.cs
+++ CountdownTimerTest/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("1a5d7f8c-b3dd-4c33-b651-f89f38d266d3")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
